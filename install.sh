c5b5b2b5348a60493c475b1084a348a6637f49fe
Andrew <andrewdymov@gmail.com>
2017-06-15 15:39:31 +0300

    Shell installation

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 apt install python3 --yes
 apt install python3-pip --yes
-apt install git --yes
 pip3 install --upgrade pip
 pip3 install requests netaddr
 pip3 install --upgrade requests
